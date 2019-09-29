c998924ce571656a553c25ddb274b21bb57cdb11
finaclemence15 <finaclemence15@gmail.com>
2019-09-29 04:47:29 +0200

    start.sh file

---
+++ start.sh
@@ -0,0 +1,5 @@
+export QUOTE_API_BASE_URL = "http://quotes.stormconsultancy.co.uk/random.json"
+export SECRET_KEY=12345
+export MAIL_USERNAME='finakagabo@gmail.com'
+export MAIL_PASSWORD='Kigali100@'
+python3.6 manage.py server
