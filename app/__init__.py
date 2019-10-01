270d148202bc5620389eac5c646b883ca78e77d6
finaclemence15 <finaclemence15@gmail.com>
2019-10-01 19:04:23 +0200

    adding profile pictures

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,8 @@ def create_app(config_name):
    from .auth import auth as auth_blueprint
    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
    
-
+   # configure UploadSet
+   configure_uploads(app,photos)
 
    bootstrap.init_app(app)
    db.init_app(app)
